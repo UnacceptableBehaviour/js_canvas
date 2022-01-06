4a92b6568c74ef8944d53624c5f3f3a48e3982d0
UnacceptableBehaviour <fernandez.simon@gmail.com>
2022-01-06 19:05:19 +0000

    arrays & loops

--- test_pages/u2_fundamentals/pattern.js
+++ test_pages/u2_fundamentals/pattern.js
@@ -22,3 +22,57 @@ context.stroke();               // start drawing
 context.beginPath();            // start contructing path
 context.arc(300,300,100, 0, 2*Math.PI);
 context.stroke();  
+
+//// draw grid of arrays 
+//for (let i=0; i < 5; i++){
+//  for (let j=0; j < 5; j++){
+//    let width = 60;
+//    let height = 60;
+//    let gap = 20;
+//    let x = 100 + (width + gap) *i;
+//    let y = 100 + (height + gap) *j;
+//    
+//    context.beginPath();            // outer rect
+//    context.rect(x,y, width,height);
+//    context.stroke();       
+//
+//    // on refresh draws inner rect in different places
+//    if (Math.random() > 0.5) {
+//      context.beginPath();            // inner rect
+//      context.rect(x+8,y+8, width-16,height-16);
+//      context.stroke();       
+//    }
+//  }
+//}
+
+// as code ^ above ^ but code tidied up
+// draw grid of arrays 
+const width = 60;
+const height = 60;
+const gap = 20;
+let x,y;
+
+for (let i=0; i < 5; i++){
+  for (let j=0; j < 5; j++){
+    x = 100 + (width + gap) *i;
+    y = 100 + (height + gap) *j;
+    
+    context.beginPath();            // outer rect
+    context.rect(x,y, width,height);
+    context.stroke();       
+
+    // on refresh draws inner rect in different places
+    if (Math.random() > 0.5) {
+      context.beginPath();            // inner rect
+      context.rect(x+8,y+8, width-16,height-16);
+      context.stroke();       
+    }
+  }
+}
+
+// introduces - array iterator
+//Array.forEach( item => {
+//   console.log(item)           
+//});
+
+
