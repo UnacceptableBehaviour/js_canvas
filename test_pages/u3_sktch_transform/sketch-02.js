e6420bce798bb87b03b7a50b2fe37cf94d03c0ae
UnacceptableBehaviour <fernandez.simon@gmail.com>
2022-01-07 16:40:31 +0000

    code

--- test_pages/u3_sktch_transform/sketch-02.js
+++ test_pages/u3_sktch_transform/sketch-02.js
@@ -1,7 +1,12 @@
 const canvasSketch = require('canvas-sketch');
+const math = require('canvas-sketch-util/math');
 
 const settings = {
   dimensions: [ 1080, 1080 ]
+};
+
+function deg2rad(deg) {
+  return (deg /180 * Math.PI);
 };
 
 const sketch = () => {
@@ -11,10 +16,13 @@ const sketch = () => {
     
     context.fillStyle = 'black';
 
-    const x = width * 0.5;
-    const y = height * 0.5;    
-    const w = width * 0.3;
-    const h = height * 0.3;
+    const cx = width * 0.5;
+    const cy = height * 0.5;    
+    const w = width * 0.01;
+    const h = height * 0.1;
+    let x,y;
+		const num = 25;
+		const radius = width * 0.3;
         
     //context.translate(x,y); // Remaps the (0,0) position on the canvas
                             // https://www.w3schools.com/tags/canvas_translate.asp
@@ -22,52 +30,25 @@ const sketch = () => {
     //context.rotate(0.3);// rotation in rad rotates around origin
                           // 2 * Math.PI rads in 360 degs
                           // 20 degs = 20 * Math.PI / 180
-    const rx = 300;
-    const ry = 300;
-    context.beginPath();
-    context.rect(rx,ry, w,h);         // <<< - - - RECT
-    //context.rect(x,y, w,h);
-    context.fill();
+    
+		for (let i = 0; i < num; i++) {
+			const slice = deg2rad(360 / num);
+			const angle = slice * i;
 
-    context.beginPath();
-    // show origin
-    context.fillStyle = 'blue';
-    context.arc(0, 0, 10, 0, 2*Math.PI);
-    context.fill();
+			x = cx + radius * Math.sin(angle);    // calc point on circle
+			y = cy + radius * Math.cos(angle);
 
-    // show rect place
-    context.beginPath();
-    context.fillStyle = 'green';
-    context.arc(rx, ry, 10, 0, 2*Math.PI);
-    context.fill();
-    
-    // show translate place
-    context.beginPath();
-    context.fillStyle = 'orange';
-    context.arc(rx + w/2, ry + h/2, 15, 0, 2*Math.PI);
-    //context.arc(0, 0, 10, 0, 2*Math.PI);
-    context.fill();    
-    
-    context.translate(rx + w/2, ry + h/2);  // centre of rect
-    context.beginPath();
-    // show origin
-    context.fillStyle = 'red';
-    context.arc(0, 0, 5, 0, 2*Math.PI);
-    context.fill();
-    
-    context.fillStyle = 'black';
-    for (let i=0; i<9; i++) {
-      context.beginPath();      
-      // place north
-      // arc(x,y, radius, start angle, end angle)
-      context.arc(0, -400, 50, 0, 2*Math.PI);
-      context.fill();
-      context.rotate(45 * Math.PI / 180)
-    }
-    
-    
-    
-    
+			context.save();
+			context.translate(x, y);
+			context.rotate(-angle);
+      
+			context.beginPath();
+			
+      context.rect(-w * 0.5, -h * 0.5, w, h);
+			context.fill();
+			context.restore();
+
+		}
   };
 };
 
