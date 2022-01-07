64d1a7c6eb8b4e222e93c8fdf4d90aef1d076ada
UnacceptableBehaviour <fernandez.simon@gmail.com>
2022-01-07 20:12:43 +0000

    save-kwik

--- test_pages/u3_sktch_transform/sketch-02.js
+++ test_pages/u3_sktch_transform/sketch-02.js
@@ -1,63 +1,84 @@
 const canvasSketch = require('canvas-sketch');
 const math = require('canvas-sketch-util/math');
+const random = require('canvas-sketch-util/random');
+
+// rainbow
+let colorBox = ['#B505B5', '#FF00AA', '#9400FF', '#3B00FF', '#0094FF', '#00E1FF',
+                '#00ED9E', '#00FF6A', '#00E203', '#A2ED00', '#F6FF00', '#FFD800',
+                '#FF7F00', '#FF3700'];
+// blue grey green
+let colorBox2 = ['#001D70', '#123082', '#294793', '#4660A3', '#0039C9', '#00B1C9',
+                '#00C181', '#3B00FF', '#368E6F', '#8DC900', '#570089', '#522BAD',
+                '#0082E0', '#3D9BC6'];
+
+// autumn brown
+let colorBox3 = ['#3A2500', '#E89E00', '#F4EC00', '#000000', '#5E2100', '#600000',
+                '#630E32', '#840919', '#AA842A', '#C17700', '#F4C300', '#93832F',
+                '#3D2409', '#332316'];
+
+let colorBox4 = ['#B505B5', '#FF00AA', '#9400FF', '#3B00FF', '#0094FF', '#00E1FF',
+                '#00ED9E', '#00FF6A', '#00E203', '#A2ED00', '#F6FF00', '#FFD800',
+                '#FF7F00', '#FF3700'];
+
+let boxPalette = [colorBox, colorBox2, colorBox3];
+let colorBoxSelector = 1;
+let colorBoxSelector2 = 1;
 
 const settings = {
-  dimensions: [ 1080, 1080 ]
-};
-
-function deg2rad(deg) {
-  return (deg /180 * Math.PI);
-};
-
-const randomRange = (min, max) => {
-  // 0 to <1 EG 0.999999 
-  return Math.random() * (max - min) + min;
+	dimensions: [ 1080, 1080 ]
 };
 
 const sketch = () => {
-  return ({ context, width, height }) => {
-    context.fillStyle = 'white';
-    context.fillRect(0, 0, width, height);
-    
-    context.fillStyle = 'black';
+	return ({ context, width, height }) => {
+		context.fillStyle = 'white';
+		context.fillRect(0, 0, width, height);
 
-    const cx = width * 0.5;
-    const cy = height * 0.5;    
-    const w = width * 0.01;
-    const h = height * 0.1;
-    let x,y;
-		const num = 12;
-		const radius = width * 0.3;
-        
-    //context.translate(x,y); // Remaps the (0,0) position on the canvas
-                            // https://www.w3schools.com/tags/canvas_translate.asp
-    
-    //context.rotate(0.3);// rotation in rad rotates around origin
-                          // 2 * Math.PI rads in 360 degs
-                          // 20 degs = 20 * Math.PI / 180
-    
+		context.fillStyle = 'black';
+
+		const cx = width  * 0.5;
+		const cy = height * 0.5;
+
+		const w = width  * 0.01;
+		const h = height * 0.55;
+		let x, y;
+
+		const num = 150;
+		const radius = width * 0.15;
+
 		for (let i = 0; i < num; i++) {
-			const slice = deg2rad(360 / num);
+			const slice = math.degToRad(360 / num);
 			const angle = slice * i;
 
-			x = cx + radius * Math.sin(angle);    // calc point on circle
+			x = cx + radius * Math.sin(angle);
 			y = cy + radius * Math.cos(angle);
 
 			context.save();
 			context.translate(x, y);
 			context.rotate(-angle);
-      context.scale(randomRange(1,4), randomRange(0.5,1.5));  // scale(x,y)
-      
-			context.beginPath();			
-      context.rect(-w * 0.5, -h * 0.5, w, h);
+			context.scale(random.range(0.1, 2), random.range(0.2, 0.5));
+
+			context.beginPath();
+      cBox = boxPalette[colorBoxSelector];
+      context.fillStyle = cBox[Math.floor(random.range(0, cBox.length - 1))];
+			context.rect(-w * 0.5, random.range(0, -h * 0.5), w, h);
 			context.fill();
-      // or
-      //context.strokeRect(-w * 0.5, -h * 0.5, w, h);  // rect border
-      
 			context.restore();
 
+			context.save();
+			context.translate(cx, cy);
+			context.rotate(-angle);
+
+			context.lineWidth = random.range(20, 40);
+
+			context.beginPath();
+      cBox = boxPalette[colorBoxSelector2];
+      context.strokeStyle = cBox[Math.floor(random.range(0, cBox.length - 1))];
+			context.arc(0, 0, radius * random.range(1.6, 3.0), slice * random.range(1, -8), slice * random.range(1, 5));
+			context.stroke();
+
+			context.restore();
 		}
-  };
+	};
 };
 
 canvasSketch(sketch, settings);
