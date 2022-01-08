efafd1bca1cceb85f6b530bad93a6a41b518bd29
UnacceptableBehaviour <fernandez.simon@gmail.com>
2022-01-08 17:49:25 +0000

    end L1

--- test_pages/u5_noise/sketch-04.js
+++ test_pages/u5_noise/sketch-04.js
@@ -1,13 +1,64 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  dimensions: [ 2048, 2048 ]
+  dimensions: [ 1080, 1080 ]
+  //animate: true
+};
+
+const params = {
+	cols: 10,
+	rows: 10,
+	scaleMin: 1,
+	scaleMax: 30,
+	freq: 0.001,
+	amp: 0.2,
+	frame: 0,
+	animate: true,
+	lineCap: 'butt',
 };
 
 const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
+    
+    const cols = params.cols;
+		const rows = params.rows;
+		const numCells = cols * rows;
+
+		const gridw = width  * 0.8;
+		const gridh = height * 0.8;
+		const cellw = gridw / cols;
+		const cellh = gridh / rows;
+		const margx = (width  - gridw) * 0.5;
+		const margy = (height - gridh) * 0.5;
+    
+    
+    for (let i = 0; i < numCells; i++) {
+			const col = i % cols;
+			const row = Math.floor(i / cols);
+
+			const x = col * cellw;
+			const y = row * cellh;
+			const w = cellw * 0.8;
+			const h = cellh * 0.8;
+      
+      
+      context.save();                     // < - - - - - - - - - - - - context save
+			context.translate(x,y);             // for each tile
+      context.translate(margx,margy);     // include the margin around the tiles
+      context.translate(cellw * 0.5, cellh * 0.5);  // translate to centre odf cell
+      
+      context.beginPath();
+			context.moveTo(w * -0.5, 0);
+			context.lineTo(w *  0.5, 0);
+			context.stroke();
+      
+      context.restore();                  // < - - - - - - - - - - - - context restore
+    }
+    
+    
+    
   };
 };
 
