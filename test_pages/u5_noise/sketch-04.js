90c0c01067f439f686041fc9bc759ba0b953d40e
UnacceptableBehaviour <fernandez.simon@gmail.com>
2022-01-10 19:49:05 +0000

    animated lines - simplex noise 2022.01.10-19.48.27.png

--- test_pages/u5_noise/sketch-04.js
+++ test_pages/u5_noise/sketch-04.js
@@ -3,9 +3,15 @@ const random = require('canvas-sketch-util/random');
 const math = require('canvas-sketch-util/math');
 const Tweakpane = require('tweakpane');
 
+
+// helpers
+const cl = (str) => {
+  console.log(str);
+}
+
 const settings = {
-  dimensions: [ 1080, 1080 ]
-  //animate: true
+  dimensions: [ 1080, 1080 ],
+  animate: true
 };
 
 // TODO retro fit params to U3 & U4 - plus the console
@@ -23,7 +29,7 @@ const params = {
 };
 
 const sketch = () => {
-  return ({ context, width, height }) => {
+  return ({ context, width, height, frame }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
     
@@ -48,11 +54,13 @@ const sketch = () => {
 			const w = cellw * 0.8;
 			const h = cellh * 0.8;
       
-			const n = random.noise2D(x, y); //(x + frame * 10, y, params.freq); // returns between -1 & 1
-			// const n = random.noise3D(x, y, f * 10, params.freq);
- 
-			const angle = n * Math.PI; // * params.amp;   // 2*PI rad in circle -1 thu 1 give whole 360
-      
+      // https://github.com/mattdesl/canvas-sketch-util/blob/master/docs/random.md#noise2D
+      // x,y co-ordinate of noise
+      // how large is the 2d array? didn't complain at 1000x1000
+			const n = random.noise2D(x + frame * 10, y, 0.001); //(x + frame * 10, y, params.freq); // returns between -1 & 1
+			// const n = random.noise3D(x, y, f * 10, params.freq);      
+			const angle = n * Math.PI * 0.2; // * params.amp;   // 2*PI rad in circle -1 thu 1 give whole 360
+            
       context.save();                     // < - - - - - - - - - - - - context save
 			context.translate(x,y);             // for each tile
       context.translate(margx,margy);     // include the margin around the tiles
@@ -65,6 +73,8 @@ const sketch = () => {
 			context.lineTo(w *  0.5, 0);
 			context.stroke();
       
+      context.beginPath();
+      
       context.restore();                  // < - - - - - - - - - - - - context restore
     }
     
@@ -74,3 +84,23 @@ const sketch = () => {
 };
 
 canvasSketch(sketch, settings);
+
+      //const g = random.noise2D(x, y)
+      //const greyScale = Math.floor( math.mapRange(g, -1,1,0,0xFFFFFF) ); 
+      //cl(`noise: ${g} - gs:${greyScale}`);
+      
+//      // add shaded square in background
+//			context.save();
+//      context.translate(x,y);             // for each tile
+//      context.translate(margx,margy);     // include the margin around the tiles
+//      context.translate(cellw * 0.5, cellh * 0.5);  // translate to centre odf cell
+//      // hollow box - works
+//			context.strokeStyle = greyScale;
+//      context.strokeRect(w * -0.5,w * -0.5,w,w);
+//      //
+//      // filledRect - No work :<
+//      context.beginPath();
+//			context.fillStyle = greyScale; // take a grey scal from 2D noise
+//			context.fillRect(w * -0.5,w * -0.5,w,w);
+//			context.fill();
+//			context.restore();
