447902474695ad5fc175bc4380fab754578ad572
UnacceptableBehaviour <fernandez.simon@gmail.com>
2022-01-26 13:32:48 +0000

    replace tabs w space

--- test_pages/u3_sktch_transform/sketch-02.js
+++ test_pages/u3_sktch_transform/sketch-02.js
@@ -29,60 +29,60 @@ let colorBoxSelectorOuterRim = 3;
 
 
 const settings = {
-	dimensions: [ 1080, 1080 ]
+  dimensions: [ 1080, 1080 ]
 };
 
 const sketch = () => {
-	return ({ context, width, height }) => {
-		context.fillStyle = 'white';
-		context.fillRect(0, 0, width, height);
+  return ({ context, width, height }) => {
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, width, height);
 
-		context.fillStyle = 'black';
+    context.fillStyle = 'black';
 
-		const cx = width  * 0.5;
-		const cy = height * 0.5;
+    const cx = width  * 0.5;
+    const cy = height * 0.5;
 
-		const w = width  * 0.01;
-		const h = height * 0.55;
-		let x, y;
+    const w = width  * 0.01;
+    const h = height * 0.55;
+    let x, y;
 
-		const num = 150;
-		const radius = width * 0.15;
+    const num = 150;
+    const radius = width * 0.15;
 
-		for (let i = 0; i < num; i++) {
-			const slice = math.degToRad(360 / num);
-			const angle = slice * i;
+    for (let i = 0; i < num; i++) {
+      const slice = math.degToRad(360 / num);
+      const angle = slice * i;
 
-			x = cx + radius * Math.sin(angle);
-			y = cy + radius * Math.cos(angle);
+      x = cx + radius * Math.sin(angle);
+      y = cy + radius * Math.cos(angle);
 
-			context.save();
-			context.translate(x, y);
-			context.rotate(-angle);
-			context.scale(random.range(0.1, 2), random.range(0.2, 0.5));
+      context.save();
+      context.translate(x, y);
+      context.rotate(-angle);
+      context.scale(random.range(0.1, 2), random.range(0.2, 0.5));
 
-			context.beginPath();
+      context.beginPath();
       cBox = boxPalette[colorBoxSelectorRadials];
       context.fillStyle = cBox[Math.floor(random.range(0, cBox.length - 1))];
-			context.rect(-w * 0.5, random.range(0, -h * 0.5), w, h);
-			context.fill();
-			context.restore();
+      context.rect(-w * 0.5, random.range(0, -h * 0.5), w, h);
+      context.fill();
+      context.restore();
 
-			context.save();
-			context.translate(cx, cy);
-			context.rotate(-angle);
+      context.save();
+      context.translate(cx, cy);
+      context.rotate(-angle);
 
-			context.lineWidth = random.range(30, 50);
+      context.lineWidth = random.range(30, 50);
 
-			context.beginPath();
+      context.beginPath();
       cBox = boxPalette[colorBoxSelectorOuterRim];
       context.strokeStyle = cBox[Math.floor(random.range(0, cBox.length - 1))];
-			context.arc(0, 0, radius * random.range(1.6, 3.0), slice * random.range(1, -8), slice * random.range(1, 5));
-			context.stroke();
+      context.arc(0, 0, radius * random.range(1.6, 3.0), slice * random.range(1, -8), slice * random.range(1, 5));
+      context.stroke();
 
-			context.restore();
-		}
-	};
+      context.restore();
+    }
+  };
 };
 
 canvasSketch(sketch, settings);
